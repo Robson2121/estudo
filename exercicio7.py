1b7de6780bab0c05f179c6529f058b68b50d33a4
Robson2121 <118029167+Robson2121@users.noreply.github.com>
2022-12-30 20:50:27 -0300

    Função de Multiplicação

---
+++ exercicio7.py
@@ -0,0 +1,16 @@
+
+while True:
+
+    valor1 = float(input('Digite o primeiro valor:'))
+    valor2 = float(input('Digite o segundo valor:'))
+
+    resultado = 0
+
+
+
+    mult = lambda resultado: valor1*valor2
+
+    print(mult(resultado))
+
+    break
+
